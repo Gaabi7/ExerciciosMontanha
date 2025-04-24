101678c231c7611ef049c7122db73dcbc0491c19
Gaabi7 <grgabriela07@gmail.com>
2025-04-23 21:45:45 -0300

    Tabela5

---
+++ LoopFor/TabelaMulti5/README.md
@@ -0,0 +1,3 @@
+# Tabela de Multiplicação 5
+
+Este programa imprime a tabela de multiplicação do número 5, de 1 até 10, usando um laço for.
---
+++ LoopFor/TabelaMulti5/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       int i;
+       int resultado;
+
+       for (i = 1; i <= 10; i++) {
+            resultado = 5*i;
+            System.out.println("5 x " + i + " = " + resultado);
+        
+       }
+    }
+}
