99f03a87b1994ae938b865726abdf8a709e68da7
Gaabi7 <grgabriela07@gmail.com>
2025-04-24 20:51:08 -0300

    Soma dos elementos em um vetor

---
+++ LoopFor/SomaElemenVetor/SomaVetor/README.md
@@ -0,0 +1,3 @@
+#  Soma de Elementos em um Vetor
+
+Este progama em Java faz a soma de todos os elementos de um vetor de 10 numeros positivos.
---
+++ LoopFor/SomaElemenVetor/SomaVetor/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] vetor = {1,2,3,4,5,6,7,8,9,10};
+        int soma = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            soma = soma + vetor[i];
+
+            System.out.println("A soma total de todos os elementos dos vetores é: " + soma);
+        }
+    }
+}
